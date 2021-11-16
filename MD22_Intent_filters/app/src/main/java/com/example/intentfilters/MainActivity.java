c25423f6e89bebcfa69d5f6c218a28d770820009
noble6 <bitcoineazy@gmail.com>
2021-11-16 16:59:26 +0300

    Intent Filters (reverse intent)

--- MD22_Intent_filters/app/src/main/java/com/example/intentfilters/MainActivity.java
+++ MD22_Intent_filters/app/src/main/java/com/example/intentfilters/MainActivity.java
@@ -6,20 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText etLName;
+    Intent intent;
+    TextView tvName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         etLName = findViewById(R.id.etLName);
+        tvName = findViewById(R.id.tvName);
     }
 
     public void onClick(View v) {
-        Intent intent;
-
         switch(v.getId()) {
             case (R.id.btnTime):
                 intent = new Intent("com.example.intent.action.showtime");
@@ -31,7 +34,19 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("lname", etLName.getText().toString());
                 startActivity(intent);
                 break;
+            case (R.id.btnName):
+                intent = new Intent(this, NameActivity.class);
+                startActivityForResult(intent, 1);
+                break;
         }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null) {return;}
+        String name = data.getStringExtra("name");
+        tvName.setText("Your name is " + name);
+    }
+
 
 }
