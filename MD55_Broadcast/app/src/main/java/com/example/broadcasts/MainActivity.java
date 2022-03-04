46969c033d5d0631ceb238322aa6e4a9aa59b127
noble6 <bitcoineazy@gmail.com>
2022-03-04 11:06:44 +0300

    MD55 "Работа с широковещательными сообщениями"

---
+++ MD55_Broadcast/app/src/main/java/com/example/broadcasts/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.broadcasts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String ALARM_MESSAGE = "Срочно пришлите кота!";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent();
+        ComponentName componentName = new ComponentName(this, MessageReceiver.class);
+        intent.putExtra("com.example.broadcast.Message", ALARM_MESSAGE);
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        intent.setComponent(componentName);
+        sendBroadcast(intent);
+    }
+}
