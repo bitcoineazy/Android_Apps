3126d1f29f5abdaebe945e4ddbf82cedc8277ef6
noble6 <bitcoineazy@gmail.com>
2022-04-12 22:42:14 +0300

    CD27 "Установка среды разработки"

---
+++ CD27_DE/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Constants from 'expo-constants';
+import {StyleSheet, View, Button, Alert, TextInput} from 'react-native';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: "",
+    }
+  }
+
+  render() {
+    return  (
+        <View style={styles.container}>
+          <View style={{
+            flexDirection:"row",
+            alignItems:"center",
+            marginHorizontal: 55,
+            borderWidth: 2,
+            marginTop: 30,
+            paddingHorizontal: 10,
+            borderColor: "#00716F",
+            borderRadius: 23,
+            paddingVertical: 2,
+          }}>
+            <TextInput
+                onChangeText={name => this.setState({name})}
+                placeholder="Введите имя"/>
+          </View>
+          <View style={{ marginTop: 30 }}>
+            <Button title="Нажми" onPress={() => Alert.alert("Ваше имя: " + this.state.name)}/>
+          </View>
+        </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
